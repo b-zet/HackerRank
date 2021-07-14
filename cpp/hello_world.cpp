4130954f0498baf0ae00ddc7fe299da699c272b4
Bartholomew <>
2021-07-15 00:15:43 +0200

    init

---
+++ cpp/hello_world.cpp
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/cpp-hello-world/problem
+// xxx
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
