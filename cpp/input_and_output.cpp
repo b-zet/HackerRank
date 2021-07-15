ca2998bcad0154f0c39a8d5c7d848ff9eefdf6ff
Bartholomew <b.zareba@email.com>
2021-07-15 17:27:36 +0200

    cpp solutions 1

--- cpp/input_and_output.cpp
+++ cpp/input_and_output.cpp
@@ -1,4 +1,4 @@
-// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem
 #include <cmath>
 #include <cstdio>
 #include <vector>
