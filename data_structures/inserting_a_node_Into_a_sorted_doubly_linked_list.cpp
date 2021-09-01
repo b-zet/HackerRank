13518b3135f47115ac06a70e91a5cf3a7b63419f
Bartholomew <51024893+b-zet@users.noreply.github.com>
2021-09-02 01:53:59 +0200

    inserting_a_node_Into_a_sorted_doubly_linked_list_v2

--- data_structures/inserting_a_node_Into_a_sorted_doubly_linked_list.cpp
+++ data_structures/inserting_a_node_Into_a_sorted_doubly_linked_list.cpp
@@ -3,32 +3,66 @@
 // solution code
 DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) {
     DoublyLinkedListNode* prev{llist};
-    DoublyLinkedListNode* node{llist->next};
-    DoublyLinkedListNode* next{node->next};
+    DoublyLinkedListNode* node{llist};
+    DoublyLinkedListNode* next{llist};
+    DoublyLinkedListNode* newNode = new DoublyLinkedListNode (data);
+
+    if (llist->next==nullptr) {
+        if (data<llist->data) {
+            newNode->next = llist;
+            llist->prev = newNode;
+            return newNode;
+        } else{
+            newNode->prev = llist;
+            llist->next=newNode;
+            return llist;
+        }
+    }
+    node = llist->next;
+    if (node->next==nullptr) {
+        if(data < prev->data){
+            newNode->next=prev;
+            prev->prev = newNode;
+            return newNode;
+        }
+        if (data < node->data) {
+            newNode->next = node;
+            newNode->prev = prev;
+            node->prev = newNode;
+            prev->next=newNode;
+            return prev;
+        } else{
+            newNode->prev = node;
+            node->next=newNode;
+            return llist;
+        }
+    }
+
+    next = node->next;
 
     if(prev->data > data){
-        DoublyLinkedListNode* newNode = new DoublyLinkedListNode (data);
         newNode->next=llist;
-        newNode->prev = nullptr;
         llist->prev = newNode;
         return newNode;
     }
-
+    if(node->data > data){
+        newNode->next=node;
+        node->prev = newNode;
+        prev->next=newNode;
+        return llist;
+    }
     while(node!=nullptr){
         if(next->data > data){
-            DoublyLinkedListNode* newNode = new DoublyLinkedListNode (data);
             node->next=newNode;
             newNode->next = next;
             newNode->prev = node;
             return llist;
         }
         if(next->next==nullptr){
-            DoublyLinkedListNode* newNode = new DoublyLinkedListNode (data);
             next->next = newNode;
             newNode->prev = next;
             return llist;
         }
-
         prev=prev->next;
         node=node->next;
         next=next->next;
