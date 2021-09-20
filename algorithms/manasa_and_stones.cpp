694279d15559d65250f9049fa2f0ba84af229a00
Bartholomew <51024893+b-zet@users.noreply.github.com>
2021-09-21 01:44:33 +0200

    manasa_and_stones

--- algorithms/manasa_and_stones.cpp
+++ algorithms/manasa_and_stones.cpp
@@ -1,50 +1,20 @@
 // https://www.hackerrank.com/challenges/manasa-and-stones/problem
 
-// solution code 28/30 points  (too slow)
+// solution code
 vector<int> stones(int n, int a, int b) {
-    vector<int> stonesCount;
-    vector<int> stonesTotal;
-    vector<int> stonesUnique;
-    int temp{0};
+    vector<int> stones;
+    int temp{a};
 
-    // recursion
-    if (n>2){
-        stonesCount = stones(n-1,a,b);
-    } else{
-        if(a<b){
-            stonesCount.push_back(a);
-            stonesCount.push_back(b);
-        } else if(a>b){
-            stonesCount.push_back(b);
-            stonesCount.push_back(a);
-        } else if(a==b) stonesCount.push_back(b);
-        return stonesCount;
+    if(a==b) {
+        stones.push_back((n-1)*a);
+        return stones;
     }
 
-    // adding new stones
-    for(int i=0;i<stonesCount.size();i++){
-        stonesTotal.push_back(stonesCount[i]+a);
-        stonesTotal.push_back(stonesCount[i]+b);
+    if(a>b){a=b;b=temp;}
+
+    for(int i=(n-1)*a;i<=(n-1)*b;i+=b-a){
+        stones.push_back(i);
     }
 
-    // sorting
-    if (n>2){
-        for (int i=0;i<stonesTotal.size();i++){
-            for(int j=0;j<stonesTotal.size()-1;j++){
-                if(stonesTotal[j+1]<stonesTotal[j]){
-                    temp = stonesTotal[j+1];
-                    stonesTotal[j+1]=stonesTotal[j];
-                    stonesTotal[j]=temp;
-                }
-            }
-        }
-    }
-
-    // delete duplicates
-    stonesUnique.push_back(stonesTotal[0]);
-    for(int i=1;i<stonesTotal.size();i++){
-        if(stonesTotal[i]!=stonesTotal[i-1]) stonesUnique.push_back(stonesTotal[i]);
-    }
-
-    return stonesUnique;
+    return stones;
 }
