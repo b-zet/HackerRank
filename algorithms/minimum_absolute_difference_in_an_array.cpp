d792b61e126d8aaa98c5c9fbf9ed651083d9408b
Bartholomew <51024893+b-zet@users.noreply.github.com>
2021-10-24 00:25:27 +0200

    minimum_absolute_difference_in_an_array

--- algorithms/minimum_absolute_difference_in_an_array.cpp
+++ algorithms/minimum_absolute_difference_in_an_array.cpp
@@ -5,12 +5,13 @@ int minimumAbsoluteDifference(vector<int> arr) {
     int result = abs(arr[1]-arr[0]);
     int temp{0};
 
-    for(int i=0;i<arr.size()-1;i++)
-        for(int j=i+1;j<arr.size();j++) {
-            temp = abs(arr[j]-arr[i]);
-            if (temp<result) result = temp;
-            if (result==0) return 0;
-        }
+    sort(begin(arr),end(arr));
+
+    for(int i=0;i<arr.size();i++){
+        temp = abs(arr[i+1]-arr[i]);
+        if (temp<result) result = temp;
+        if (result==0) return 0;
+    }
 
     return result;
 }
