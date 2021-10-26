60048fab17589bfe911f7397ba543a55b9801e7c
Bartholomew <51024893+b-zet@users.noreply.github.com>
2021-10-27 00:13:01 +0200

    game_of_stones

---
+++ algorithms/game_of_stones.cpp
@@ -0,0 +1,6 @@
+// https://www.hackerrank.com/challenges/game-of-stones-1/problem
+
+// solution code
+string gameOfStones(int n) {
+    if (n%7>=2 & n%7<=6) return "First"; else return "Second";
+}
